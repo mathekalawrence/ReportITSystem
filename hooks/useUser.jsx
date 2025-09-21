4760b877591f98e8e9f61ad36baf5de266a94b3f
mathekalawrence <mathekalawrence96@gmail.com>
2025-09-21 13:27:37 +0300

    ....

--- hooks/useUser.jsx
+++ hooks/useUser.jsx
@@ -3,4 +3,10 @@ import { UserContext } from '../contexts/UserContext'
 
 export function useUser(){
     const context = useContext(UserContext)
+
+    if(!context){
+        throw new Error("useUser must be used within a UserProvider")
+    }
+
+    return context
 }
